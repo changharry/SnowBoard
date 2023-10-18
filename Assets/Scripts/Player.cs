db62c79baba0ba9542b57a2f25168d69b6bf6ffa
changharry <harrychangzh@gmail.com>
2023-10-18 00:06:08 -0700

    add speed booster

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,34 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    float speed = 20f;
+    float boostSpeed = 30f;
     Rigidbody2D playerRigidbody2D;
+    SurfaceEffector2D surfaceEffector2D;
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        rotatePlayer();
+        boostPlayer();
+    }
+
+    void boostPlayer()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            surfaceEffector2D.speed = boostSpeed;
+        } else {
+            surfaceEffector2D.speed = speed;
+        }
+    }
+
+    void rotatePlayer() {
         if (Input.GetKey(KeyCode.A)) {
             playerRigidbody2D.AddTorque(torqueAmount);
         }
