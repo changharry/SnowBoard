eb520c902ca9de5de6b9474979486ee22858654e
changharry <harrychangzh@gmail.com>
2023-10-19 19:54:49 -0700

    fix double play

--- Assets/Scripts/FinishPoint.cs
+++ Assets/Scripts/FinishPoint.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class FinishPoint : MonoBehaviour
 {
     [SerializeField] ParticleSystem finishEffect;
+    bool hasEffectPlayed = false;
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Player") {
+        if (other.tag == "Player" && !hasEffectPlayed) {
             finishEffect.Play();
             GetComponent<AudioSource>().Play();
+            hasEffectPlayed = true;
             Invoke("restartGame", 1f); 
         }
     }
